3393ab0124b1cde49b18d0829a2c55b56d68e494
Ryuichi Ueda <ryuichiueda@gmail.com>
2017-04-13 20:18:14 +0900

    Add cloud-init purge

--- raspimouse_book_ubuntu_init.bash
+++ raspimouse_book_ubuntu_init.bash
@@ -2,13 +2,15 @@
 
 tmp=/tmp/$$
 
-cat /boot/firmware/config.txt 						|
-sudo tee /boot/firmware/config.txt.org 					|
-sed 's/device_tree_address=0x100/device_tree_address=0x02008000/'	|
-sed 's/device_tree_end=0x8000/#&/' 	> $tmp-config
-
-sudo mv $tmp-config /boot/firmware/config.txt
-
 sudo systemctl disable apt-daily.service
 sudo systemctl disable apt-daily.timer
 sudo apt purge unattended-upgrades
+
+cat /boot/firmware/config.txt                                           |
+sudo tee /boot/firmware/config.txt.org                                  |
+sed 's/device_tree_address=0x100/device_tree_address=0x02008000/'       |
+sed 's/device_tree_end=0x8000/#&/'      > $tmp-config
+
+sudo mv $tmp-config /boot/firmware/config.txt
+
+sudo apt purge cloud-init
